9554e493b3c1bb46f52be379cd2645c652c3fb08
kytro <kytro@paindance.com>
2023-08-10 15:14:34 +1000

    Update unmapped_drugs.sql

--- inst/sql/sql_server/checks/unmapped_drugs.sql
+++ inst/sql/sql_server/checks/unmapped_drugs.sql
@@ -3,7 +3,7 @@
 SELECT *
 FROM (
 	select ROW_NUMBER() OVER(ORDER BY count_big(drug_exposure_id) DESC) AS ROW_NUM,
-       drug_source_value as "Source Value",
+       drug_source_value as `Source Value`,
        floor((count_big(drug_exposure_id)+99)/100)*100 as "#Records",
        floor((count_big(distinct person_id)+99)/100)*100 as "#Subjects"
        from @cdmDatabaseSchema.drug_exposure where drug_concept_id = 0
