ba8c25ef808e1a13ca2f61f00d543a40da66fb2c
kytro <kytro@paindance.com>
2023-08-10 15:24:27 +1000

    Update mapping_levels_drugs.sql

--- inst/sql/sql_server/checks/mapping_levels_drugs.sql
+++ inst/sql/sql_server/checks/mapping_levels_drugs.sql
@@ -1,11 +1,11 @@
 -- Levels at which drugs are mapped
 
-select concept_class_id as "Class",
-       count_big( drug_exposure_id) as "#Records",
-       count_big(distinct person_id) as "#Patients",
-       count_big(distinct drug_source_value) as "#Source Codes"
+select concept_class_id as `Class`,
+       count_big( drug_exposure_id) as `#Records`,
+       count_big(distinct person_id) as `#Patients`,
+       count_big(distinct drug_source_value) as `#Source Codes`
 from @cdmDatabaseSchema.drug_exposure
 join @vocabDatabaseSchema.concept on drug_concept_id=concept_id
 where concept.domain_id = 'Drug'
 group by concept_class_id
-order by "#Source Codes" DESC
+order by `#Source Codes` DESC
